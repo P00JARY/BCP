d4aa44feacc8c2a63834dd850034887191638fd8
P00JARY <ashilpoojari2212@gmail.com>
2022-11-27 13:44:27 +0530

    new change

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,6 @@ function Home() {
     return (
         <>
         </>
-
     );
 }
 export default Home
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../CSS/Login.css";
+
 
 function LoginForm() {
   function Login(){
